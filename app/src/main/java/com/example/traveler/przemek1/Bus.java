1150493dad9970d82f71ab7bb6eb039b6734ee92
220986@student.pwr.edu.pl <Pldstu#59>
2018-01-29 14:52:59 +0100

    Build 3.4

--- app/src/main/java/com/example/traveler/przemek1/Bus.java
+++ app/src/main/java/com/example/traveler/przemek1/Bus.java
@@ -45,76 +45,69 @@ public class Bus extends AppCompatActivity implements AsyncResponse {
             tekst3.setText("Rejestracja: " + value3);
         }
 
-
         HashMap postData2 = new HashMap();
         postData2.put("mobile1", "android1");
         postData2.put("idbusa", value1);
         PostResponseAsyncTask task = new PostResponseAsyncTask(this, postData2);
         task.execute("http://traveler95.nazwa.pl/jeden/client/zawartoscbus.php");
-
-
     }
 
     ArrayList<Autobus> BusList = new ArrayList<>();
 
-
     @Override
-    public void processFinish(String result)
-    {
+    public void processFinish(String result) {
+        if (result == "") {
 
-        int i = 1;
-        for (String retval1 : result.split(">"))
-        {
-            i=1;
-            final Autobus bus = new Autobus("", "", "");
-            for (String retval2 : retval1.split("-")) {
-                if (i == 1) {
-                    bus.setRejestracja(retval2);
-                }
-                if (i == 2) {
-                    bus.setIdentyfikator(retval2);
-                }
-                if (i == 3) {
-                    bus.setMarka(retval2);
-                }
-                i++;
-            }
-            BusList.add(bus);
-        }
-        ListView mListView = (ListView) findViewById(R.id.listView);
-
-
-
-        BusListAdapter adapter = new BusListAdapter(this, R.layout.adapter_view_layout, BusList);
-        mListView.setAdapter(adapter);
-
-
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+        } else
             {
-
-                Autobus transporter=BusList.get(position);
-                String idkogos=transporter.getIdentyfikator();
-                String marka=transporter.getMarka();
-                String rejestracja=transporter.getRejestracja();
-
-                switch (position)
-                {
-
-
-                    default:
-                        Intent in = new Intent(Bus.this, Zmien_ilosc.class);
-                        in.putExtra("tekst1",idkogos);
-                        in.putExtra("tekst2",marka);
-                        in.putExtra("tekst3",rejestracja);
-                        startActivity(in);
-                        break;
+            int i = 1;
+            for (String retval1 : result.split(">")) {
+                i = 1;
+                final Autobus bus = new Autobus("", "", "");
+                for (String retval2 : retval1.split("-")) {
+                    if (i == 1) {
+                        bus.setRejestracja(retval2);
+                    }
+                    if (i == 2) {
+                        bus.setIdentyfikator(retval2);
+                    }
+                    if (i == 3) {
+                        bus.setMarka(retval2);
+                    }
+                    i++;
                 }
+                BusList.add(bus);
             }
-        });
+            ListView mListView = (ListView) findViewById(R.id.listView);
+
+
+            BusListAdapter adapter = new BusListAdapter(this, R.layout.adapter_view_layout, BusList);
+            mListView.setAdapter(adapter);
+
+
+            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    Autobus transporter = BusList.get(position);
+                    String idkogos = transporter.getIdentyfikator();
+                    String marka = transporter.getMarka();
+                    String rejestracja = transporter.getRejestracja();
+
+                    switch (position) {
+
+
+                        default:
+                            Intent in = new Intent(Bus.this, Zmien_ilosc.class);
+                            in.putExtra("tekst1", idkogos);
+                            in.putExtra("tekst2", marka);
+                            in.putExtra("tekst3", rejestracja);
+                            startActivity(in);
+                            break;
+                    }
+                }
+            });
+        }
     }
 }
 
--- app/src/main/java/com/example/traveler/przemek1/Zmien_ilosc.java
+++ app/src/main/java/com/example/traveler/przemek1/Zmien_ilosc.java
@@ -1,25 +1,43 @@
 package com.example.traveler.przemek1;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Zmien_ilosc extends AppCompatActivity {
+
+public class Zmien_ilosc extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    Button p10;
+    Button p1;
+    Button m10;
+    Button m1;
+    int ilosc=0;
+    TextView textView9;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bus_zmien);
+        setContentView(R.layout.activity_zmien_ilosc);
 
 
+        p10 = (Button) findViewById(R.id.p10);
+        p10.setOnClickListener(this);
+        p1 = (Button) findViewById(R.id.p1);
+        p1.setOnClickListener(this);
+        m10 = (Button) findViewById(R.id.m10);
+        m10.setOnClickListener(this);
+        m1 = (Button) findViewById(R.id.m1);
+        m1.setOnClickListener(this);
+        textView9 = (TextView)findViewById(R.id.textView9);
 
-
-
-
-
-
-
-
+        textView9.setText("wtff");
 
 
         //zmiana rozmiaru okienka
@@ -31,5 +49,25 @@ public class Zmien_ilosc extends AppCompatActivity {
 
 
 
+    }
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.p10:
+                ilosc=ilosc+10;
+                String iloscstring = ilosc + "";
+                textView9.setText(iloscstring);
+                break;
+
+            case R.id.p1:
+                textView9.setText("ss");
+                break;
+            case R.id.m10:
+                break;
+            case R.id.m1:
+                break;
+
+        }
     }
 }
